0298d142ced19224d9565287fc63baa277eded1e
TilliWilli_Home <tilliwilli@gmail.com>
2017-09-24 18:53:15 +0300

    container -> Page

--- Client/Component/Page/exhibitCreatorPage.jsx
+++ Client/Component/Page/exhibitCreatorPage.jsx
@@ -1,16 +1,16 @@
 "use strict";
 import React from "react";
 import Exhibit from "../Exhibit/index.jsx";
+import {connect} from "react-redux";
+import {SubmitNewExhibit, ShowErrorWindow} from "../../App/ac.js";
 
-export default class ExhibitCreatorPage extends React.Component
-{
-    constructor(props){
-        super(props);
-    }
+const S2P = state => ({
+    data: state.draft,
+    language: state.language,
+});
+const D2P = dsp => ({
+    SubmitNewExhibit: exhibitData=>dsp(SubmitNewExhibit(exhibitData)),
+    ShowErrorWindow: error=>dsp(ShowErrorWindow(error))
+});
 
-    render(){
-        return (
-            <Exhibit />
-        );
-    }
-}
+export default connect(S2P,D2P)(Exhibit);
--- Client/Component/Page/exhibitEditPage.jsx
+++ Client/Component/Page/exhibitEditPage.jsx
@@ -1,9 +1,16 @@
 "use strict";
 import React from "react";
-import ExhibitEdit from "../ExhibitEdit/index.jsx";
+import Exhibit from "../Exhibit/index.jsx";
+import {connect} from "react-redux";
+import {SubmitNewExhibit, ShowErrorWindow} from "../../App/ac.js";
 
-export default function ExhibitEditPage(p){
-    return (
-        <ExhibitEdit />
-    );
-};
+const S2P = state => ({
+    data: state.exhibitEdit,
+    language: state.language,
+});
+const D2P = dsp => ({
+    SubmitNewExhibit: exhibitData=>dsp(SubmitNewExhibit(exhibitData)),
+    ShowErrorWindow: error=>dsp(ShowErrorWindow(error))
+});
+
+export default connect(S2P,D2P)(Exhibit);
