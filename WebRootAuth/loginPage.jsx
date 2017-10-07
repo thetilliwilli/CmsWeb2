2fafa06585ebe474907b13510d84d8e2ac730719
TilliWilli_Home <tilliwilli@gmail.com>
2017-10-07 23:53:22 +0300

    начал работать на Оформелением страници авторизации

--- WebRootAuth/loginPage.jsx
+++ WebRootAuth/loginPage.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Card, CardTitle, CardText } from "material-ui/Card";
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import Paper from 'material-ui/Paper';
 
 export default class LoginPage extends React.Component {
   constructor(props) {
@@ -55,15 +56,15 @@ export default class LoginPage extends React.Component {
 
   render() {
     return (
-      <Card className="LoginPage">
-          <CardTitle title="Login" />
-          <form action="/" onSubmit={this.OnSubmit}>
-            <TextField autoFocus name="login" value={this.state.login} onChange={this.OnChange} hintText="Login" floatingLabelText="Type Login" /><br />
-            <TextField name="password" value={this.state.password} onChange={this.OnChange} type="password" hintText="Password" floatingLabelText="Type Password" /><br />
-            <RaisedButton type="submit" label="Create New Account" primary />
+      <Paper className="LoginPage" zDepth={3} style={{width:"50%", height:"50%", margin:"auto", display:"flex"}} >
+          <form action="/" onSubmit={this.OnSubmit} style={{width:"75%", margin:"auto"}} >
+            <CardTitle title="Login" style={{width:"100%", margin:"auto"}} />
+            <TextField style={{width:"100%",}} autoFocus name="login" value={this.state.login} onChange={this.OnChange} hintText="Login" floatingLabelText="Type Login" /><br />
+            <TextField style={{width:"100%",}} name="password" value={this.state.password} onChange={this.OnChange} type="password" hintText="Password" floatingLabelText="Type Password" /><br />
+            <RaisedButton style={{width:"100%",}} type="submit" label="Create New Account" primary />
             {/* <CardText>Doesn't have an account? <Link to={'/signup'}>Signup</Link></CardText> */}
           </form>
-      </Card>
+      </Paper>
     );
   }
 }
