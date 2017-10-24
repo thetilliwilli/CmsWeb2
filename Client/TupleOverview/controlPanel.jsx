1fb05999b7d4a725461273c3c98a5eda5a57652a
thetilliwilli <tilliwilli3@gmail.com>
2017-10-24 18:09:34 +0300

    fix tuple overview search field

--- Client/TupleOverview/controlPanel.jsx
+++ Client/TupleOverview/controlPanel.jsx
@@ -13,10 +13,13 @@ export default class ControlPanel extends React.Component
     render(){
         return (
             <div className="TupleOverview_ControlPanel">
-                <TextField 
-                    value={this.props.filterValue} onChange={this.props.OnChange}
-                    floatingLabelText="Поиск по имени экспоната" floatingLabelFixed={true}
-                />
+                <div style={{width:"80%", margin:"0 auto 0 auto"}} >
+                    <TextField 
+                        value={this.props.filterValue} onChange={this.props.OnChange}
+                        floatingLabelText="Поиск по имени экспоната" floatingLabelFixed={true}
+                        fullWidth
+                    />
+                </div>
                 <br/>
                 <RaisedButton label="Обновить список" onClick={this.props.OnRefresh}/>
             </div>
