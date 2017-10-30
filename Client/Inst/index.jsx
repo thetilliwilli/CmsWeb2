d783e89a1338b80a4406ca2da5cc3cca2425a435
thetilliwilli <tilliwilli3@gmail.com>
2017-10-30 20:13:14 +0300

    поправил еррор в случае невыбранного устройства

--- Client/Inst/index.jsx
+++ Client/Inst/index.jsx
@@ -5,13 +5,15 @@ import InstSelector from "./selector.jsx";
 import InstEditor from "./editor.jsx";
 import ControlPanel from "./cp.jsx";
 
+const Warning = (props) => <span style={{color:"Chocolate "}}>{props.message || ""}</span>;
+
 export default class Inst extends React.Component
 {
     constructor(props){
         super(props);
 
         this.state = {
-            selectedInstId: 0,
+            selectedInstId: null,
             filterValue: "",
         };
         
@@ -31,6 +33,7 @@ export default class Inst extends React.Component
         const typeFilter = this.props.domain === "tag"
             ? "exhibit"
             : this.props.domain;
+        const theInst = this.props.instList.find(i => i.id === this.state.selectedInstId);
         return (
             <div style={{width:"100%", height:"100%", display:"flex", flexWrap:"wrap"}} >
                 <div className="Inst.ControlPanel.layout" style={{width:"100%", height:"10%"}}>
@@ -38,7 +41,7 @@ export default class Inst extends React.Component
                 </div>
                 {
                     this.props.instList.filter(ex => ex.type ? ex.type === typeFilter : false).length === 0
-                        ? <span style={{color:"Chocolate "}}>В системе не зарегистрировано ни одного Устройства</span>
+                        ? <Warning message="В системе не зарегистрировано ни одного Устройства"/>
                         :
                             <div className="Inst.EditorSelector.layout" style={{width:"100%", height:"90%", display:"flex", flexWrap:"wrap"}}>
                                 <div style={{width:"40%", height:"100%", wordBreak:"break-all", overflow:"auto"}} >
@@ -50,12 +53,17 @@ export default class Inst extends React.Component
                                     />
                                 </div>
                                 <div style={{width:"60%", height:"100%", padding:"0px 0px 0px 6px"}} >
-                                    <InstEditor
-                                        data={this.props.instList.find(i => i.id === this.state.selectedInstId) || {}}
-                                        InstChange={this.props.InstChange}
-                                        SubmitUpdate={this.props.SubmitUpdate}
-                                        SubmitDelete={this.props.SubmitDelete}
-                                    />
+                                    {
+                                        theInst
+                                            ?
+                                                <InstEditor
+                                                    data={this.props.instList.find(i => i.id === this.state.selectedInstId) || {}}
+                                                    InstChange={this.props.InstChange}
+                                                    SubmitUpdate={this.props.SubmitUpdate}
+                                                    SubmitDelete={this.props.SubmitDelete}
+                                                />
+                                            : <Warning message="Выберите устройства из списка"/>
+                                    }
                                 </div>
                             </div>
                 }
