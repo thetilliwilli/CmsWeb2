983612c492992cde93307de16014794f3dc962bf
thetilliwilli <tilliwilli3@gmail.com>
2017-11-14 19:40:26 +0300

    api port: 8080 -> 7777 +PB

--- Client/Module/config.js
+++ Client/Module/config.js
@@ -3,7 +3,7 @@ export default {
     api:{
         schema: "http",
         hostname: (new URL(window.location.href).hostname),
-        port: 8080,
+        port: 7777,
         url: "",
         get rootUrl(){ return `${this.schema}://${this.hostname}:${this.port}/${this.url}` }
     },
