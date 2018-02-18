7a2ca588df3da17a70a65f8372150d6305f16685
TilliWilli_Home <tilliwilli@gmail.com>
2018-02-18 19:47:30 +0300

    добавил табики для ExhibitOverview Page в режиме Portrait

--- Client/ExhibitOverview/index.jsx
+++ Client/ExhibitOverview/index.jsx
@@ -3,10 +3,13 @@ import React from "react";
 import RaisedButton from 'material-ui/RaisedButton';
 import {Card} from "material-ui/Card";
 import CircularProgress from 'material-ui/CircularProgress';
+import {Tabs, Tab} from 'material-ui/Tabs';
 
 import ControlPanel from "./controlPanel.jsx";
 import ExhibitList from "./exhibitList.jsx";
 import InstProvider from "../Inst/provider.jsx";
+
+import util from "../Module/util.js";
 
 class ExhibitOverview extends React.Component
 {
@@ -36,24 +39,42 @@ class ExhibitOverview extends React.Component
     }
 
     render(){
-        return (
-            <div className="ExhibitOverview" style={{width:"100%", height:"100%", display:"flex", flexWrap:"wrap"}}>
-                <div style={{width:"45%", height:"100%", padding: "20px 20px 20px 20px", borderRight:"1px solid lightgrey"}} >
-                    <InstProvider OnChangeComplex={this.OnChangeComplex} EditEntity={this.props.EditExhibit}/>
-                </div>
-                <div style={{width:"55%", height:"100%", display:"flex", flexWrap:"wrap", padding: "20px 20px 20px 20px", borderLeft:"1px solid lightgrey"}} >
-                    <div style={{width:"100%", height:"10%"}} >
-                        <ControlPanel OnChange={this.OnFilterChange} filterValue={this.state.filter} OnRefresh={this.props.FetchOverview}/>
-                    </div>
-                    <div style={{width:"100%", height:"90%", overflow:"auto"}} >
-                        <ExhibitList
-                            OnBadgeSelect={this.OnBadgeSelect} OnDelete={this.props.DeleteExhibit}
-                            exhibitList={this.props.model} filter={this.state.filter} filterValue={this.state.filter} complex={this.state.complex}
-                        />
-                    </div>
-                </div>
+        const widths = util.IfLandscape({inst:"45%", itemList:"55%"}, {inst:"100%", itemList:"100%"});
+        const rightContentHeights = util.IfLandscape({control:"14%", list:"86%"}, {control:"initial", list:"initial"});
+        const LeftContent = <div style={{width:widths.inst, height:"100%", padding: "20px 20px 20px 20px", borderRight:"1px solid lightgrey"}} >
+            <InstProvider OnChangeComplex={this.OnChangeComplex} EditEntity={this.props.EditExhibit}/>
+        </div>;
+        const RightContent = <div style={{width:widths.itemList, height:"100%", display:"flex", flexWrap:"wrap", padding: "20px 20px 20px 20px", borderLeft:"1px solid lightgrey"}} >
+            <div style={{width:"100%", height:rightContentHeights.control}} >
+                <ControlPanel OnChange={this.OnFilterChange} filterValue={this.state.filter} OnRefresh={this.props.FetchOverview}/>
             </div>
-        );
+            <div style={{width:"100%", height:rightContentHeights.list, overflow:"auto"}} >
+                <ExhibitList
+                    OnBadgeSelect={this.OnBadgeSelect} OnDelete={this.props.DeleteExhibit}
+                    exhibitList={this.props.model} filter={this.state.filter} filterValue={this.state.filter} complex={this.state.complex}
+                />
+            </div>
+        </div>;
+        const WholeContent = util.isLandscape
+            ? 
+                <div className="ExhibitOverview" style={{width:"100%", height:"100%", display:"flex", flexWrap:"wrap"}}>
+                    {LeftContent}
+                    {RightContent}
+                </div>
+            :
+                <div className="ExhibitOverview" style={{width:"100%", height:"100%", display:"flex", flexWrap:"wrap"}}>
+                    <Tabs>
+                        <Tab label="Список контента">
+                            {RightContent}
+                            
+                        </Tab>
+                        <Tab label="Устройства">
+                            {LeftContent}
+                        </Tab>
+                    </Tabs>
+                </div>
+            ;
+        return WholeContent;
     }
 }
 
